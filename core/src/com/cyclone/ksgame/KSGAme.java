bb2bd9a677ff3073daf5b2deeda4d0c9a2cdfbee
dreamscape1 <caladbolg87@gmail.com>
2014-11-25 17:28:52 +0800

    Initialize

--- core/src/com/cyclone/ksgame/KSGAme.java
+++ core/src/com/cyclone/ksgame/KSGAme.java
@@ -1,4 +1,4 @@
-package com.mygdx.game;
+package com.cyclone.ksgame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class KSGame extends ApplicationAdapter {
+public class KSGAme extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
--- desktop/src/com/cyclone/ksgame/desktop/DesktopLauncher.java
+++ desktop/src/com/cyclone/ksgame/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
-package com.mygdx.game.desktop;
+package com.cyclone.ksgame.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.KSGame;
+import com.cyclone.ksgame.KSGAme;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new KSGame(), config);
+		new LwjglApplication(new KSGAme(), config);
 	}
 }
